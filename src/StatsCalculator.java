a8a486dfec180204856730921d919ea19fc0ddb7
Anvay Bhanap <anvay.bhanap@gmail.com>
2020-10-02 23:25:23 -0700

    added Sample Size and Confidence Interval equations

--- src/StatsCalculator.java
+++ src/StatsCalculator.java
@@ -1,34 +1,71 @@
 import java.util.*;
-public class StatsCalculator{
-	
-	
-	public static void sop (Object x) {
+
+public class StatsCalculator {
+
+	public static void sop(Object x) {
 		System.out.println(x);
 	}
 
-    public static void main(String args[]){
-		
-		Scanner scan = new Scanner (System.in);
+	public static void main(String args[]) {
+
+		Scanner scan = new Scanner(System.in);
 		sop("Welcome to the Stats Calculator");
 		sop("Please enter the appropriate letter for the type of calculation you want to do:");
-		sop(" B - Basic Stats,"
-				+ "\n ...,"
-				+ "\n S - Sample Size");
-		
+		sop(" B - Basic Stats," + "\n ...," + "\n S - Sample Size");
+
 		String response = scan.next();
-		
-		
-		if (scan.next() == "B") {
-			//mean, median, mode, range
+
+		if (response.equals("B")) {
+			// mean, median, mode, range
 		}
-		
-		//... code
-		
-		if (scan.next() == "S"){
-			//insert SampleSize code
-		}		
-		
-	
-        
-    }
+
+		// different stats functions code
+
+		if (response.equals("S")) {
+			double p, q, z, w;
+			int n;
+
+			sop("Enter p value: ");
+			p = scan.nextDouble();
+
+			q = 1 - p;
+
+			sop("Enter z value: ");
+			z = scan.nextDouble();
+
+			sop("Enter width: ");
+			w = scan.nextDouble();
+
+			double nPlus;
+
+			nPlus = (2 * Math.pow(z, 2) * p * q - Math.pow(z, 2) * Math.pow(w, 2)
+					+ Math.sqrt(
+							4 * Math.pow(z, 4) * p * q * (p * q - Math.pow(w, 2)) + Math.pow(w, 2) * Math.pow(z, 4)))
+					/ Math.pow(w, 2);
+
+//			nMinus = (2*Math.pow(z, 2)*p*q - Math.pow(z, 2)*Math.pow(w,2)- Math.sqrt(4*Math.pow(z, 4)*p*q*(p*q-Math.pow(w, 2))
+//					+Math.pow(w, 2)*Math.pow(z, 4))) / Math.pow(w, 2);
+
+			sop("The necessary sample size is " + nPlus);
+
+			double percent = p * 100;
+			percent = Math.round(percent * 10.00) / 10.00;
+
+			sop("Enter size: ");
+			n = scan.nextInt();
+
+			sop("\nIf a " + percent + " % of " + n + " individuals have done something,");
+
+			double ciMinus = p - z * Math.sqrt((p * q) / n);
+			double ciPlus = p + z * Math.sqrt((p * q) / n);
+
+			// this rounds the CI values to 4 decimal places
+			ciMinus = Math.round(ciMinus * 10000.00) / 10000.00;
+			ciPlus = Math.round(ciPlus * 10000.00) / 10000.00;
+
+			sop("\tthe confidence interval is (" + ciMinus + "," + ciPlus + ")");
+
+		}
+
+	}
 }

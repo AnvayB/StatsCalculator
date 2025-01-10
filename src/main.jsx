1621ade12784efa352ced32fa8d5516555e14b04
Anvay Bhanap <anvay.bhanap@gmail.com>
2025-01-10 12:10:50 +0530

    MUI components

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
+/* eslint-disable no-unused-vars */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import StatsCalculator from './StatsCalculator.jsx'
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
